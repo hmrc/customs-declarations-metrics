7825bba860a75661d1f76552d00b6598c65583c0
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2019-02-19 12:10:51 +0000

    CDD-1889: changed from playReactiveMongo to simpleReactiveMongo & updated some other dependencies

--- test/integration/MetricsMongoRepoSpec.scala
+++ test/integration/MetricsMongoRepoSpec.scala
@@ -27,10 +27,8 @@ import reactivemongo.play.json.JsObjectDocumentWriter
 import uk.gov.hmrc.customs.api.common.logging.CdsLogger
 import uk.gov.hmrc.customs.declarations.metrics.model.{ConversationId, ConversationMetrics, MetricsConfig}
 import uk.gov.hmrc.customs.declarations.metrics.repo.{MetricsMongoRepo, MetricsRepoErrorHandler, MongoDbProvider}
-import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.mongo.MongoSpecSupport
 import uk.gov.hmrc.play.test.UnitSpec
-import util.MockitoPassByNameHelper.PassByNameVerifier
 import util.TestData._
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -48,7 +46,7 @@ class MetricsMongoRepoSpec extends UnitSpec
 
   val twoWeeksInSeconds = 1209600
 
-  private val mongoDbProvider = new MongoDbProvider {
+  private val mongoDbProvider: MongoDbProvider = new MongoDbProvider {
     override val mongo: () => DB = self.mongo
   }
 
@@ -64,8 +62,8 @@ class MetricsMongoRepoSpec extends UnitSpec
     await(repository.drop)
   }
 
-  private def collectionSize: Int = {
-    await(repository.collection.count())
+  private def collectionSize: Long = {
+    await(repository.collection.count(None, None, 0, None, mongoDbProvider.mongo().connection.options.readConcern))
   }
 
   private def selector(conversationId: ConversationId) = {
@@ -80,7 +78,7 @@ class MetricsMongoRepoSpec extends UnitSpec
       saveResult shouldBe true
       collectionSize shouldBe 1
 
-      val findResult = await(repository.collection.find(selector(DeclarationConversationId)).one[ConversationMetrics]).get
+      val findResult = await(repository.collection.find(selector(DeclarationConversationId), None).one[ConversationMetrics]).get
       findResult.conversationId.id.toString shouldBe "dff783d7-44ee-4836-93d0-3242da7c225f"
       findResult.events.head.eventType.eventTypeString shouldBe "DECLARATION"
       findResult.events.head.eventStart should not be None
@@ -96,7 +94,7 @@ class MetricsMongoRepoSpec extends UnitSpec
       await(repository.updateWithFirstNotification(NotificationConversationMetric))
       collectionSize shouldBe 1
 
-      val findResult = await(repository.collection.find(selector(DeclarationConversationId)).one[ConversationMetrics]).get
+      val findResult = await(repository.collection.find(selector(DeclarationConversationId), None).one[ConversationMetrics]).get
       findResult.events.size shouldBe 2
       findResult.conversationId.id.toString shouldBe "dff783d7-44ee-4836-93d0-3242da7c225f"
       findResult.events.head.eventType.eventTypeString shouldBe "DECLARATION"
@@ -128,7 +126,7 @@ class MetricsMongoRepoSpec extends UnitSpec
       val caught = intercept[IllegalStateException](await(repository.updateWithFirstNotification(NotificationConversationMetric)))
       caught.getMessage shouldBe "event data not updated for ConversationMetric(dff783d7-44ee-4836-93d0-3242da7c225f,Event(EventType(NOTIFICATION),2014-10-23T00:36:14.123Z,2014-10-23T00:36:18.123Z))"
       collectionSize shouldBe 1
-      val findResult = await(repository.collection.find(selector(DeclarationConversationId)).one[ConversationMetrics]).get
+      val findResult = await(repository.collection.find(selector(DeclarationConversationId), None).one[ConversationMetrics]).get
       findResult.events.size shouldBe 2
     }
 
