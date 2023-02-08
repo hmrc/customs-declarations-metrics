c03d80050b5758695f913a9bd7072277012d5bcf
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-02-08 12:02:24 +0000

    Libraries & plugins upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,21 +3,21 @@ import sbt.{compilerPlugin, _}
 object AppDependencies {
 
   private val testScope = "test,it"
-  private val hmrcMongoVersion = "0.73.0"
+  private val hmrcMongoVersion = "0.74.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "customs-api-common" % "1.57.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion,
-    "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full,
-    compilerPlugin("com.github.ghik" % "silencer-plugin"    % "1.7.5" cross CrossVersion.full),
+    "uk.gov.hmrc"                                %% "customs-api-common" % "1.57.0",
+    "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-play-28" % hmrcMongoVersion,
+    "com.github.ghik"                            %  "silencer-lib"       % "1.7.12" % Provided cross CrossVersion.full,
+    compilerPlugin("com.github.ghik" %  "silencer-plugin"    % "1.7.12" cross CrossVersion.full),
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "customs-api-common" % "1.57.0" % testScope classifier "tests",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % testScope,
-    "com.github.tomakehurst" % "wiremock-standalone" % "2.27.1" % testScope,
-    "org.scalatestplus" %% "mockito-3-4" % "3.2.9.0" % testScope,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % testScope,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.35.10"  % testScope
+    "uk.gov.hmrc"            %% "customs-api-common"      % "1.57.0"         % testScope classifier "tests",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"          % testScope,
+    "com.github.tomakehurst" %  "wiremock-standalone"     % "2.27.2"         % testScope,
+    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"        % testScope,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % testScope,
+    "com.vladsch.flexmark"   % "flexmark-all"             % "0.35.10"        % testScope
   )
 }
