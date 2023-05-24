cc7026643b91a7ceb172a2518c44474748e508b0
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-05-24 13:55:32 +0100

    DCWL-809 Pulls out apiCommonVersion value

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,18 @@ object AppDependencies {
 
   private val testScope = "test,it"
   private val hmrcMongoVersion = "0.74.0"
+  private val apiCommonVersion = "1.59.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"                                %% "customs-api-common" % "1.59.0",
+    "uk.gov.hmrc"                                %% "customs-api-common" % apiCommonVersion,
     "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-play-28" % hmrcMongoVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "customs-api-common"      % "1.59.0"         % testScope classifier "tests",
+    "uk.gov.hmrc"            %% "customs-api-common"      % apiCommonVersion % testScope classifier "tests",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"          % testScope,
     "com.github.tomakehurst" %  "wiremock-standalone"     % "2.27.2"         % testScope,
-    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"        % testScope,
+    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"       % testScope,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % testScope,
     "com.vladsch.flexmark"   % "flexmark-all"             % "0.35.10"        % testScope
   )
