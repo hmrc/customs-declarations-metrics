813f70d3a87d0991310f6df97b2100f59fef3da5
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2023-09-06 09:46:33 +0100

    DCWL-1151 Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,7 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"            %% "customs-api-common"      % apiCommonVersion % testScope classifier "tests",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"          % testScope,
-    "com.github.tomakehurst" %  "wiremock-standalone"     % "3.0.1"         % testScope,
+    "org.wiremock"            %  "wiremock-standalone"     % "3.0.2"         % testScope,
     "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"       % testScope,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % testScope,
     "com.vladsch.flexmark"   % "flexmark-all"             % "0.35.10"        % testScope
