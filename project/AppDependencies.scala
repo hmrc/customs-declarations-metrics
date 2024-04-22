b7ef706b8fdd8ff8ae02edc0df7587173ffa2dac
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2024-04-22 16:30:14 +0100

    [DCWL-2275] Code clean up

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,11 +12,11 @@ object AppDependencies {
     "uk.gov.hmrc"                                %% s"bootstrap-backend$playSuffix" % bootstrapVersion,
     "org.typelevel"                              %% "cats-core"                    % "2.10.0",
     "uk.gov.hmrc.mongo"                          %% s"hmrc-mongo$playSuffix"        % hmrcMongoVersion,
-    "com.typesafe.play" %% "play-json-joda" % "2.8.1"
+    "com.typesafe.play"                          %% "play-json-joda"               % "2.8.1"
   )
 
   val test = Seq(
-    "org.scalatestplus" %% "scalatestplus-mockito" % "1.0.0-M2" % Test,
+    "org.scalatestplus"                          %% "scalatestplus-mockito"        % "1.0.0-M2"                      % Test,
     "uk.gov.hmrc"                                %% s"bootstrap-test$playSuffix"    % bootstrapVersion                % Test,
     "uk.gov.hmrc.mongo"                          %% s"hmrc-mongo-test$playSuffix"   % "1.8.0"                         % Test,
     "org.scalamock"                              %% "scalamock"                    % scalamockVersion                % Test
