70f8c272a52bcbbd25a31f04cf54602705d3e1aa
Jake Reid <75027964+JakeReid2020@users.noreply.github.com>
2024-04-23 16:05:09 +0100

    [DCWL-2275] - Cleaning up dependencies/AppDependencies.scala
    Co-authored-by: Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,6 +2,27 @@ import sbt._
 
 object AppDependencies {
 
+  val playSuffix       = "-play-30"
+  val bootstrapVersion = "8.0.0"
+  val hmrcMongoVersion = "1.8.0"
+  val scalamockVersion = "5.2.0"
+
+  val compile = Seq(
+    "uk.gov.hmrc"        %% s"bootstrap-backend$playSuffix" % bootstrapVersion,
+    "org.typelevel"      %% "cats-core"                     % "2.10.0",
+    "uk.gov.hmrc.mongo"  %% s"hmrc-mongo$playSuffix"        % hmrcMongoVersion,
+    "com.typesafe.play"  %% "play-json-joda"                % "2.8.1"
+  )
+
+  val test = Seq(
+    "org.scalatestplus"  %% "scalatestplus-mockito"        % "1.0.0-M2" ,
+    "uk.gov.hmrc"        %% s"bootstrap-test$playSuffix"   % bootstrapVersion,
+    "uk.gov.hmrc.mongo"  %% s"hmrc-mongo-test$playSuffix"  % hmrcMongoVersion,
+    "org.scalamock"      %% "scalamock"                    % scalamockVersion
+  ).map(_ % Test)
+}
+
+
   val playSuffix = "-play-30"
 
   val bootstrapVersion = "8.0.0"
