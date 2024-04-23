b47f27d25f51b4fa73edad65235b08320541d902
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-04-23 16:57:08 +0100

    DCWL-2275 Removed unneeded dependencies as they are provided by bootstrap-test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,13 +11,10 @@ object AppDependencies {
     "uk.gov.hmrc"        %% s"bootstrap-backend$playSuffix" % bootstrapVersion,
     "org.typelevel"      %% "cats-core"                     % "2.10.0",
     "uk.gov.hmrc.mongo"  %% s"hmrc-mongo$playSuffix"        % hmrcMongoVersion,
-    "com.typesafe.play"  %% "play-json-joda"                % "2.8.1"
   )
 
   val test = Seq(
-    "org.scalatestplus"  %% "scalatestplus-mockito"        % "1.0.0-M2" ,
     "uk.gov.hmrc"        %% s"bootstrap-test$playSuffix"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"  %% s"hmrc-mongo-test$playSuffix"  % hmrcMongoVersion,
-    "org.scalamock"      %% "scalamock"                    % scalamockVersion
   ).map(_ % Test)
 }
