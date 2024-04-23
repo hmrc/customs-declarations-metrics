91c4416e32366220631fcc5bdcaaffbc659bd7c7
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-04-23 16:39:51 +0100

    DCWL-2275 Updated some versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val playSuffix       = "-play-30"
   val bootstrapVersion = "8.0.0"
   val hmrcMongoVersion = "1.9.0"
-  val scalamockVersion = "5.2.0"
+  val scalamockVersion = "6.0.0"
 
   val compile = Seq(
     "uk.gov.hmrc"        %% s"bootstrap-backend$playSuffix" % bootstrapVersion,
